dafd788213f4c21fa27b9b22bd2d9ef7ee302a29
Aabid <sayeedaabid0@gmail.com>
2022-01-30 17:17:29 +0530

    day 5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,61 +1,64 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utills/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png", 
-              fit: BoxFit.cover),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Welcome",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png", 
+                fit: BoxFit.cover),
+              SizedBox(
+                height: 20.0,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                decoration: InputDecoration(
-                   hintText: "Enter Username",
-                   labelText: "Username"
-                   ),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  hintText: "Password"
-                  ),
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               SizedBox(
                 height: 20.0,
               ),
-              ElevatedButton(
-                child: Text("Login"),
-                style:TextButton.styleFrom(),
-                onPressed: (){
-                   print("Hi Aabid");
-                },
-                 ),
-                ],
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                  decoration: InputDecoration(
+                     hintText: "Enter Username",
+                     labelText: "Username"
+                     ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Password"
+                    ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style:TextButton.styleFrom(minimumSize: Size(120,40)),
+                  onPressed: (){
+                     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  },
+                   ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
